f6e883fc12aa93adc925a6139ead701d0de38dfe
Alan <alanhdiniz@hotmail.com>
2017-08-09 15:31:08 -0300

    Add files via upload

---
+++ state0.js
@@ -0,0 +1,9 @@
+var bionicLocker = {};
+
+bionicLocker.state0 = function(){};
+bionicLocker.state0.protoype =
+{
+    preload: function(){},
+    create:  function(){},
+    update:  function(){}
+};
